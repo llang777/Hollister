5b2731f1a2bd40f173b74b7a5f96259dc16c64cc
llang777 <146140759+llang777@users.noreply.github.com>
2024-08-16 00:13:12 -0400

    initial test again

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function showPopup(date) {
-    document.getElementById('popup-date').textContent = date;
-    document.getElementById('popup-container').style.display = 'flex';
-}
-
-function hidePopup() {
-    document.getElementById('popup-container').style.display = 'none';
-}
